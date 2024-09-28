208628371195b6f8a146393106d06939749c083d
leeyounghuncom <web3dao@kakao.com>
2024-09-28 19:20:56 +0900

    쿠키테스트

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.querySelector('.kakao-login').addEventListener('click', function () {
-    location.href = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=c39ec02ba741d3bb7d91cc57847a9566&redirect_uri=http://localhost:8000/api/v1/accounts/auth/kakao/callback/';
+    location.href = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=c39ec02ba741d3bb7d91cc57847a9566&redirect_uri=https://a846-49-163-214-103.ngrok-free.app/api/v1/accounts/auth/kakao/callback/';
 });
