db9e2ad030a71670919d995cc0089c74a26dd16f
leeyounghuncom <web3dao@kakao.com>
2024-09-28 17:40:07 +0900

    클릭링크

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('.kakao-login').addEventListener('click', function () {
+    location.href = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=c39ec02ba741d3bb7d91cc57847a9566&redirect_uri=http://localhost:8000/api/v1/accounts/auth/kakao/callback/';
+});
